7b7ec61ce7d3aa4385674fecad5cfd4fe0e3a864
郭勝威 <guosw.cs13@nycu.edu.tw>
2024-10-16 10:16:16 +0800

    feature: add dialog in [user].vue

--- nuxt/components/FileCard.vue
+++ nuxt/components/FileCard.vue
@@ -3,30 +3,51 @@
   <v-card>
     <v-card-title>{{ file.filename }}</v-card-title>
     <v-list>
-      <v-list-item 
-      v-for="(obj, index) in file.content" 
-      :key="index" 
-      :value="obj"
-      :class="obj.type"
+      <v-list-item
+        v-for="(obj, index) in file.content"
+        :key="index"
+        :value="obj"
+        :class="obj.type"
+        @click="openDialog(index)"
       >
-        <v-list-item-content >{{ obj.url }}</v-list-item-content>
+        <v-list-item-title>{{ obj.url }} {{ index }}</v-list-item-title>
+        <v-dialog v-model="dialog[index]" max-width="90%">
+          <v-card>
+            <v-card-title>URL: {{ obj.url }}</v-card-title>
+            <v-card-text>
+              <v-textarea v-model="obj.content" label="Summary" required readonly max-rows="20" auto-grow no-resize></v-textarea>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="red darken-1" text @click="closeDialog(index)">Cancel</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
       </v-list-item>
     </v-list>
   </v-card>
-
 </template>
 
 <script setup>
-defineProps({
+const props = defineProps({
   file: {
     type: Object,
     required: true,
   },
 });
+
+const dialog = reactive(Array(props.file.content.length).fill(false));
+
+const openDialog = (index) => {
+  dialog[index] = true;
+};
+
+const closeDialog = (index) => {
+  dialog[index] = false;
+};
 </script>
 
 <style scoped>
-/* You can add additional styles if needed */
 .url {
   border-radius: 0.5cm !important;
   background: lightblue;
